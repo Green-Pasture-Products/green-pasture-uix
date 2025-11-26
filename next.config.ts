f521a608b22f15a20b12de0cb732e81044ddc7c4
Abayomi Omosehin <omosehin14@gmail.com>
2025-11-26 13:51:12 +0100

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	reactStrictMode: true,
+		// 🚫 Disable ESLint during production build
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
 	images: {
 		domains: ["images.unsplash.com", "unsplash.com"],
 	},
