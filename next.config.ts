15551e64f01c3fc6f6977d4a20c031c3a13d1cd3
Abayomi Omosehin <omosehin14@gmail.com>
2026-09-16 20:49:56 +0100

    Simplify Cloudflare production build

--- next.config.ts
+++ next.config.ts
@@ -17,25 +17,6 @@ const nextConfig: NextConfig = {
 			{ protocol: "https", hostname: "unsplash.com" },
 		],
 	},
-	webpack(config, { dev }) {
-		if (!dev && config.optimization?.minimizer) {
-			config.optimization.minimizer.forEach((plugin: any) => {
-				if (plugin.constructor.name === "TerserPlugin") {
-					if (plugin.options?.terserOptions?.compress) {
-						plugin.options.terserOptions.compress.drop_console = true;
-						plugin.options.terserOptions.compress.pure_funcs = [
-							"console.log",
-							"console.info",
-							"console.debug",
-							"console.warn",
-							"console.error",
-						];
-					}
-				}
-			});
-		}
-		return config;
-	},
 };
 
 export default nextConfig;
