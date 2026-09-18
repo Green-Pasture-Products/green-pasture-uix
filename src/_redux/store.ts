04520a6395192d5220a8dab815f42747a94ea1da
Victor Chidi <victorblessed9@gmail.com>
2026-09-18 16:49:10 +0100

    fix(store): use SSR-safe storage adapter and declare type module in package.json

--- src/_redux/store.ts
+++ src/_redux/store.ts
@@ -1,7 +1,7 @@
 // LIBRARY COMPONENTS
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 import {
 	FLUSH,
 	REHYDRATE,
@@ -11,6 +11,22 @@ import {
 	REGISTER,
 } from "redux-persist";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+const createNoopStorage = () => {
+	return {
+		getItem(_key: any) {
+			return Promise.resolve(null);
+		},
+		setItem(_key: any, value: any) {
+			return Promise.resolve(value);
+		},
+		removeItem(_key: any) {
+			return Promise.resolve();
+		},
+	};
+};
+
+const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
 
 // CUSTOM COMPONENTS
 import rootReducer from "./reducers";
